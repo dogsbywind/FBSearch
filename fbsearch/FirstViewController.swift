b4214e6111580833bdeb61e9b3c00edc755d24c9
Jibiao Shen <js@Jibiaos-MacBook-Air.local>
2017-04-17 14:36:35 -0700

    Initial Commit

---
+++ fbsearch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  fbsearch
+//
+//  Created by Jibiao Shen on 4/17/17.
+//  Copyright © 2017 Jibiao Shen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
